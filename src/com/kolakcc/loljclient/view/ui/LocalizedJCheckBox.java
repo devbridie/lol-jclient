ed7e129b86343da2a993dbd0b324091a013c8806
KolakCC <KolakCC@gmail.com>
2013-09-05 16:01:56 +0200

    Added a localized JCheckbox

---
+++ src/com/kolakcc/loljclient/view/ui/LocalizedJCheckBox.java
@@ -0,0 +1,36 @@
+package com.kolakcc.loljclient.view.ui;
+
+import javax.swing.JCheckBox;
+
+import com.kolakcc.loljclient.util.LocaleMessages;
+
+public class LocalizedJCheckBox extends JCheckBox {
+	String key;
+	
+	public LocalizedJCheckBox() {
+		super(LocaleMessages.generalMessages.getString("loading"));
+		this.key = "loading";
+	}
+	
+	public LocalizedJCheckBox(String key) {
+		super(LocaleMessages.generalMessages.getString(key));
+		this.key = key;
+	}
+	
+	public LocalizedJCheckBox(LocaleMessages messages, String key) {
+		super(messages.getString(key));
+		this.key = key;
+	}
+	
+	public void setKey(String key) {
+		super.setText(LocaleMessages.generalMessages.getString(key));
+		this.key = key;
+	}
+	
+	public void setKey(LocaleMessages messages, String key) {
+		super.setText(messages.getString(key));
+		this.key = key;
+	}
+	
+	public String getKey() { return key; }
+}
